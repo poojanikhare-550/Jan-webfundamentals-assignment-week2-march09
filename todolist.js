ad9dea90d8ca0e8d86f75e8ddc37dc3dad08cdd0
poojanikhare-550 <95541750+poojanikhare-550@users.noreply.github.com>
2022-03-13 19:15:21 +0530

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,20 @@
+function show_name(){
+    let my_name="Pooja";
+    my_name ="changed";
+    const op_element = document.getElementById('name_op');
+    op_element.innerText = my_name;
+
+    const container_div = document.getElementById('name_op');
+    op_element.innerText = my_name;
+    cons container_div = document.getElementById('container');
+    container_div.style.backgroundColor="red";
+    container_div.style.fontFamily="cursive";
+    container_div.style.boxshadow="2px px 3px black";
+    container_div.style.display="block";
+    console.log(container_div.classList);
+    container_div.classList.replace("class","class3");
+}function
+function my_img_fn(){
+    document.getElementById('some_Text').style.display="none";
+    alert("image clicked");
+}
